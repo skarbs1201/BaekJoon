30ac152294c6b67b606a342dadf16642c9354a39
유남균 <skarbs1201@naver.com>
2022-03-21 14:18:48 +0000

    2차원 ArrayList, DFS

--- src/project/BaekJoon_13023.java
+++ src/project/BaekJoon_13023.java
@@ -7,13 +7,16 @@ import java.util.StringTokenizer;
 import java.util.ArrayList;
 
 public class BaekJoon_13023 {
+    static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+    static boolean[] visited;
+    static boolean status = false;
     public static void baekJoon_13023() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        visited =  new boolean[N];
         
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>(N);
         for (int i = 0; i < N; i++) {
             list.add(new ArrayList<Integer>());
         }
@@ -24,7 +27,30 @@ public class BaekJoon_13023 {
             list.get(a).add(b);
             list.get(b).add(a);
         }
-        System.out.println(list.get(0));
+        
+        for (int i = 0; i < N; i++) {
+            dfs(i, 1);
+        }
+        System.out.println(status ? "1" : "0");
         br.close();
+    }
+    
+    public static void dfs(int i, int depth) {
+        for (int k = 0; k < 8; k++) {
+            System.out.print(visited[k] + " ");
+        }
+        System.out.println("");
+        if (depth == 5) {
+            status = true;
+            return;
+        }
+        
+        visited[i] = true;
+        for (int j : list.get(i)) {
+            if (visited[j] != true) {
+                dfs(j, depth + 1);
+            }
+        }
+        visited[i] = false;
     }
 }
